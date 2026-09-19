7dea6c3b2433240d6f1a8a4d1dec1b0371f22bc2
Shambhubainada <bainadashambhu@gmail.com>
2026-09-19 14:01:08 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='fsd-sabarmati-v1';
+const ASSETS=['./','./FSD_Sabarmati_Stack_Moisture.html','./manifest.webmanifest','./farmer_dvara_farming.gif','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./FSD_Sabarmati_Stack_Moisture.html')))));
