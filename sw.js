490e3441f365908f61202947f1577b2c833015a6
Shambhubainada <bainadashambhu@gmail.com>
2026-09-19 14:17:23 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fsd-sabarmati-v1';
-const ASSETS=['./','./FSD_Sabarmati_Stack_Moisture.html','./manifest.webmanifest','./farmer_dvara_farming.gif','./icon-192.png','./icon-512.png'];
+const CACHE='fsd-sabarmati-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./farmer_dvara_farming.gif','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./FSD_Sabarmati_Stack_Moisture.html')))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;}).catch(()=>caches.match('./index.html')))); });
